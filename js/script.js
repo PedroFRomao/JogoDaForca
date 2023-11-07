62d24d28c0fa3b382afcba7ac97598a6ab060f6b
Pedro Romão <pedroferraz.romao@gmail.com>
2023-11-06 22:56:20 -0300

    alteracao no jogo

--- js/script.js
+++ js/script.js
@@ -21,15 +21,26 @@ function desenharForca(erros) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     // Desenhar a forca base
+    ctx.fillStyle="#";
     ctx.beginPath();
-    ctx.moveTo(20, 380);
-    ctx.lineTo(100, 380);
-    ctx.stroke();
+    ctx.moveTo(70, 380);
+    ctx.lineTo(120, 380);
+    ctx.lineTo(120, 60);/*altura*/
+    ctx.lineTo(300, 60);
+    ctx.lineTo(300, 90);
+    ctx.lineTo(150, 90);/*base cima*/
+    ctx.lineTo(150, 380);
+    ctx.lineTo(300, 380);/*base*/
+    ctx.lineTo(400, 380);
+    ctx.lineTo(400, 410);
+    ctx.lineTo(70, 410);
+    ctx.closePath();
+    ctx.fill();
 
     if (erros >= 1) {
         // Desenhar a cabeça
         ctx.beginPath();
-        ctx.arc(60, 75, 25, 0, Math.PI * 2);
+        ctx.arc(270, 300, 25, 0, Math.PI * 2);
         ctx.stroke();
     }
 
